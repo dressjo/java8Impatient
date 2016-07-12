1a46e821b6e64187c47d947cfb3d52ea916ae05b
Johannes Dressie <johannes.dressie@gmail.com>
2016-07-12 18:42:51 +0200

    unused imports removed

--- src/main/java/ch1/ex4.java
+++ src/main/java/ch1/ex4.java
@@ -1,9 +1,6 @@
 package ch1;
 
 import java.io.File;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.Arrays;
 
 import utils.ArrayUtils;
